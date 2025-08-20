3bc113de67d9266556d508b7ffc73dbafebc34b5
jijinbei <hallucigenia2002@gmail.com>
2025-08-20 17:48:53 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -278,10 +278,10 @@ const ThreeFishScene = () => {
     camera.position.z = 100;
 
     // 照明を追加
-    const ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
+    const ambientLight = new THREE.AmbientLight(0xffffff, 2.5);
     scene.add(ambientLight);
 
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 1.5);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 3.5);
     directionalLight.position.set(1, 1, 1);
     scene.add(directionalLight);
 
